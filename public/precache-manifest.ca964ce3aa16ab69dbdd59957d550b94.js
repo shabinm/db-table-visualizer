73fedb6bf02becb8cc1b803ef9c5e53cdd10485e
Shabin Muhammed <shabinmohd@gmail.com>
2022-06-24 08:29:01 +0400

    React updated

---
+++ public/precache-manifest.ca964ce3aa16ab69dbdd59957d550b94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9167996b9bd6bfdc8a66",
+    "url": "/static/css/main.91bbc184.chunk.css"
+  },
+  {
+    "revision": "9167996b9bd6bfdc8a66",
+    "url": "/static/js/main.9167996b.chunk.js"
+  },
+  {
+    "revision": "a74c80cacc647194bf26",
+    "url": "/static/css/1.071e053c.chunk.css"
+  },
+  {
+    "revision": "a74c80cacc647194bf26",
+    "url": "/static/js/1.a74c80ca.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7535727b176b6fc368910d0eda6fe491",
+    "url": "/index.html"
+  }
+];
